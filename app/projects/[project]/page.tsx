528cc436a87ff22c4c166020c44e77a5e7cdb524
RobinTheHooter <culkkulk222@gmail.com>
2026-01-19 12:50:44 +0530

    made page dynamic

--- app/projects/[project]/page.tsx
+++ app/projects/[project]/page.tsx
@@ -1,3 +1,6 @@
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 import { getAllProjects, getProject } from '@/lib/contentful';
 // import Projects from '@/components/projects';
@@ -11,35 +14,35 @@ import NotAvailable from './_components/not-available';
 type PageProps = { params: Promise<{ project: string }> };
 
 export async function generateStaticParams() {
-	const allProjects = await getAllProjects({ limit: 10, isDraftMode: true });
-	const slugs = allProjects.map((project: any) => ({
-		slug: project.slug,
-	}));
+  const allProjects = await getAllProjects({ limit: 10, isDraftMode: false });
+  const slugs = allProjects.map((project: any) => ({
+    slug: project.slug,
+  }));
 
-	return slugs;
+  return slugs;
 }
 
 export default async function Page(props: PageProps) {
-	const project = await getProject((await props.params).project);
+  const project = await getProject((await props.params).project);
 
-	if (!project) return <NotAvailable />;
+  if (!project) return <NotAvailable />;
 
-	return (
-		<div className='px-4 mt-4 page'>
-			<Header {...project} />
-			<main>
-				{project.youtubeUrl && <Video url={project.youtubeUrl} />}
-				<section>
-					{documentToReactComponents(project.details.json as any)}
-				</section>
-			</main>
-			{/* TODO: Filter out current detail project */}
-			{/* <aside className='[&>*]:px-0'>
+  return (
+    <div className='px-4 mt-4 page'>
+      <Header {...project} />
+      <main>
+        {project.youtubeUrl && <Video url={project.youtubeUrl} />}
+        <section>
+          {documentToReactComponents(project.details.json as any)}
+        </section>
+      </main>
+      {/* TODO: Filter out current detail project */}
+      {/* <aside className='[&>*]:px-0'>
 				<Projects
 					title='Other Projects With Details'
 					description='These projects have a detailed explantion of what they do and what I learnt.'
 				/>
 			</aside> */}
-		</div>
-	);
+    </div>
+  );
 }
